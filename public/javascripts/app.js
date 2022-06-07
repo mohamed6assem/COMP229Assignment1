1f78486cd77788bcc5e5d8cc03989ea8519f2b6c
mohamed6assem <91697911+mohamed6assem@users.noreply.github.com>
2022-06-06 23:20:03 -0400

    Assignment1 final submission before deploy to Heroku
    i have done everything except deploying to heroku and adding a few comments

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,14 @@
-console.log('app script is working.');
+function captureContactInfo() {
+    var emailInput = document.getElementById("inputEmail");
+    var nameInput = document.getElementById("inputName");
+    var phoneInput = document.getElementById("inputPhone");
+    var subjectInput = document.getElementById("inputSubject");
+    var messsageInput = document.getElementById("txtMessage");
+    
+}
+
+var submit = document.getElementsByClassName("btn btn-primary mb-2");
+
+    submit.addEventListener("click", captureContactInfo, false);
+
+
