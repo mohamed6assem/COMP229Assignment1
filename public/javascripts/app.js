b0cfb8cde71b836bf3c900ba5937196fe973268a
mohamed6assem <91697911+mohamed6assem@users.noreply.github.com>
2022-06-06 23:32:03 -0400

    final version of assignment 1
    all done. ready to deploy to heroku

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,6 @@
+//Javascript for Assignment1 COMP229 Summer 2022 - By Mohamed Assem - 301163010 - June 6, 2022
+//captureContactInfo function captures all information on contact me page and is activated by clicking
+//submit button.
 function captureContactInfo() {
     var emailInput = document.getElementById("inputEmail");
     var nameInput = document.getElementById("inputName");
